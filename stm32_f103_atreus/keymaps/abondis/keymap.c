f5158c88ed95e2bcdfe05e850a0f24e0745c8d81
Aurélien Bondis <aurelien.bondis@gmail.com>
2018-05-28 10:54:49 -0400

    add LALT

--- stm32_f103_atreus/keymaps/abondis/keymap.c
+++ stm32_f103_atreus/keymaps/abondis/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_Q,           KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,      KC_I,     KC_O,     KC_P,
 SFT_T(KC_A),    KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,      KC_K,     KC_L,     SFT_T(KC_SCLN),
 KC_Z,           KC_X,     KC_C,     KC_V,     KC_B,                         KC_N,     KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,
-LT(NUM,KC_ESC), KC_TAB,   XXXXXXX, KC_LGUI,  KC_BSPC,  KC_RALT,   KC_LCTL,  KC_SPC,   XXXXXXX,   KC_MINS,  KC_QUOT,  LT(MOU, KC_ENT)
+LT(NUM,KC_ESC), KC_TAB,   XXXXXXX, KC_LGUI,  KC_BSPC,  KC_RALT,   KC_LCTL,  KC_SPC,   KC_LALT,   KC_MINS,  KC_QUOT,  LT(MOU, KC_ENT)
        ),
 /*
  * FIXME: KC_SHIFT

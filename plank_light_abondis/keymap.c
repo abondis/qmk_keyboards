34caadc57f9b28563cdf8f2e08d2afaf5dd8c851
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-10-13 23:01:19 -0400

    steno: move rows one higher

--- plank_light_abondis/keymap.c
+++ plank_light_abondis/keymap.c
@@ -168,9 +168,9 @@ KC_BTN1, _______, RESET  , _______, _______, _______, _______ , _______, _______
  * `-----------------------------------------------------------------------------------'
  */
 [_PLOVER] = {
-  {STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC },
   {STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR },
   {XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR },
+  {STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC },
   {EXT_PLV, XXXXXXX, XXXXXXX, STN_A,   STN_O,   XXXXXXX, XXXXXXX, STN_E,   STN_U,   STN_PWR, STN_RE1, STN_RE2}
 },
 
@@ -234,9 +234,9 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
 { RGB_BLUE  },{ RGB_BLUE },{ RGB_BLUE },{ RGB_BLUE },{ RGB_BLUE },{          },{},{          },{ RGB_BLUE },{ RGB_BLUE },{ RGB_BLUE },{ RGB_BLUE },{  RGB_BLUE },
 },
 [_PLOVER] = {
+{ RGB_TEAL },{          },{          },{          },{          },{ RGB_TEAL  },    { RGB_TEAL },{          },{          },{          },{          },{  RGB_TEAL},
+{ RGB_BLK  },{          },{          },{          },{          },{ RGB_TEAL  },    { RGB_TEAL },{          },{          },{          },{          },{  RGB_TEAL},
 { RGB_TEAL },{ RGB_TEAL },{ RGB_TEAL },{ RGB_TEAL },{ RGB_TEAL },{ RGB_TEAL  },    { RGB_TEAL },{ RGB_TEAL },{ RGB_TEAL },{ RGB_TEAL },{ RGB_TEAL },{  RGB_TEAL},
-{ RGB_TEAL },{          },{          },{          },{          },{           },    {          },{          },{          },{          },{          },{  RGB_TEAL},
-{ RGB_BLK  },{          },{          },{          },{          },{           },    {          },{          },{          },{          },{          },{  RGB_TEAL},
 { RGB_TEAL },{ RGB_BLK  },{ RGB_BLK  },{          },{          },{ RGB_BLK},{RGB_BLK},{RGB_BLK},{          },{          },{ RGB_TEAL },{ RGB_TEAL },{  RGB_TEAL},
 }
     /* [0] = { */

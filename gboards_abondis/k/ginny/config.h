2e70ee0e6258b9d44e958ce3f72c0cff9d0c891e
Aurélien Bondis <aurelien.bondis@gmail.com>
2020-11-11 11:59:36 -0500

    add feather pinout

--- gboards_abondis/k/ginny/config.h
+++ gboards_abondis/k/ginny/config.h
@@ -48,7 +48,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 5
 /* #define MATRIX_ROW_PINS { F4, B2, B4, B5 } */
 /* #define MATRIX_COL_PINS { D0, D4, C6, D7, E6, B6, B3, B1, F7, F6, F5 } */
+// pins 13, 12
 #define MATRIX_ROW_PINS { C7, D6 }
+// pins 11, 10, 9, 6, 5
 #define MATRIX_COL_PINS { B7, B6, B5, D7, C6 }
 #define DIODE_DIRECTION COL2ROW
 
